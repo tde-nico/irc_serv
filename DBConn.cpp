f7c2f7fb4d0390c65e6fcecb5d6900e144322602
tde-nico <101901057+tde-nico@users.noreply.github.com>
2024-01-03 22:51:43 +0100

    db connector fxed and updated

--- DBConn.cpp
+++ DBConn.cpp
@@ -1,28 +1,35 @@
 #include "DBConn.hpp"
 
-DBConn::DBConn(
-			std::string const &hostname,
-			std::string const &port,
-			std::string const &user,
-			std::string const &password,
-			std::string const &dbname)
+DBConn::DBConn(std::string const &hostname,
+	std::string const &port, std::string const &user,
+	std::string const &password, std::string const &dbname)
 {
-	std::string	buf;
-
-	buf = fmt_str(
+	this->setup(fmt_str(
 		"host=%s port=%s user=%s password=%s dbname=%s",
-		hostname,
-		port,
-		user,
-		password,
-		dbname
-	);
-	this->conn = PQconnectdb(buf.c_str());
+		hostname.c_str(), port.c_str(), user.c_str(), password.c_str(), dbname.c_str()
+	));
+}
+
+DBConn::DBConn(const char *hostname,
+	const char *port, const char *user,
+	const char *password, const char *dbname)
+{
+	this->setup(fmt_str(
+		"host=%s port=%s user=%s password=%s dbname=%s",
+		hostname, port, user, password, dbname
+	));
+}
+
+void	DBConn::setup(std::string const &connect)
+{
+	this->conn = PQconnectdb(connect.c_str());
 	if (PQstatus(this->conn) != CONNECTION_OK)
 	{
-		std::cerr << fmt_str("Error DBConn: %s\n", PQerrorMessage(this->conn));
+		console_log(fmt_str("Error DBConn: %s\n", PQerrorMessage(this->conn)), 0);
 		this->finish();
 	}
+	this->status = 0;
+	console_log("DB Connected");
 }
 
 DBConn::~DBConn()
@@ -32,8 +39,12 @@ DBConn::~DBConn()
 
 void	DBConn::finish()
 {
+	if (this->conn == nullptr)
+		return ;
+	console_log("DB Disconnected");
 	PQfinish(this->conn);
 	this->conn = nullptr;
+	this->status = 1;
 }
 
 PGresult	*DBConn::exec(std::string const &cmd)
@@ -42,18 +53,18 @@ PGresult	*DBConn::exec(std::string const &cmd)
 
 	if (this->conn == nullptr)
 	{
-		std::cerr << "No DB Connection\n";
+		console_log("No DB Connection\n", 0);
 		return (nullptr);
 	}
 	res = PQexec(this->conn, cmd.c_str());
 	if (PQresultStatus(res) != PGRES_COMMAND_OK)
 	{
-		std::cerr << fmt_str(
+		console_log(fmt_str(
 			"Error DBConn: cmd:%s status:%s err:%s\n",
-			cmd,
+			cmd.c_str(),
 			PQresStatus(PQresultStatus(res)),
 			PQresultErrorMessage(res)
-		);
+		), 0);
 		PQclear(res);
 		this->finish();
 		return (nullptr);
@@ -67,17 +78,17 @@ PGresult	*DBConn::query(std::string const &cmd)
 
 	if (this->conn == nullptr)
 	{
-		std::cerr << "No DB Connection\n";
+		console_log("No DB Connection\n", 0);
 		return (nullptr);
 	}
 	res = PQexec(this->conn, cmd.c_str());
 	if (PQresultStatus(res) != PGRES_TUPLES_OK)
 	{
-		std::cerr << fmt_str(
+		console_log(fmt_str(
 			"Error DBConn: cmd:%s err:%s\n",
-			cmd,
+			cmd.c_str(),
 			PQresultErrorMessage(res)
-		);
+		), 0);
 		PQclear(res);
 		this->finish();
 		return (nullptr);
--- DBConn.hpp
+++ DBConn.hpp
@@ -8,6 +8,15 @@
 #include <cstdio>
 #include <cstring>
 #include <cstdarg>
+#include <time.h>
+
+
+#define DB_HOST "127.0.0.1"
+#define DB_PORT "5432"
+#define DB_USER "postgres"
+#define DB_PASS "postgres"
+#define DB_NAME "irc"
+
 
 inline std::string fmt_str(const char* format, ...)
 {
@@ -34,23 +43,62 @@ inline std::string fmt_str(const char* format, ...)
 	return (buf);
 }
 
+
 class DBConn
 {
 	private:
 		PGconn	*conn;
+
+		void	setup(std::string const &connect);
 		void	finish();
 
 	public:
+		int		status;
+
 		DBConn(
 			std::string const &hostname,
 			std::string const &port,
 			std::string const &username,
 			std::string const &password,
 			std::string const &dbname);
+		DBConn(
+			const char *hostname,
+			const char *port,
+			const char *username,
+			const char *password,
+			const char *dbname);
 		~DBConn();
 
 		PGresult *exec(std::string const &cmd);
 		PGresult *query(std::string const &cmd);
 };
+
+#include <algorithm>
+
+extern DBConn	*db;
+
+inline void	console_log(std::string const &msg, int save=1)
+{
+	time_t		raw_time;
+	struct tm	*time_info;
+	char		buffer[80];
+
+	time(&raw_time);
+	time_info = localtime(&raw_time);
+
+	strftime(buffer, sizeof(buffer), "%d-%m-%Y %H:%M:%S", time_info);
+	std::string now(buffer);
+	std::string strip = msg.c_str();
+	strip.erase(std::remove_if(strip.begin(), strip.end(),
+                    [](char c) { return c == '\r' || c == '\n'; }),
+                    strip.end());
+	std::cout << "[" << now << "] " << strip << std::endl;
+	if (db != nullptr && save)
+	{
+		PGresult *res = db->exec(fmt_str(
+			"INSERT INTO logs (log, date) VALUES ('%s', '%s')", strip.c_str(), now.c_str()));
+		PQclear(res);
+	}
+}
 
 #endif
